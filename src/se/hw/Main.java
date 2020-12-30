dacd52eca1529f9dd4fc77c15f65c36594d2b6ed
yi10235512 <yi10235512@gmail.com>
2020-12-30 14:12:24 +0800

    Added main function

---
+++ src/se/hw/Main.java
@@ -0,0 +1,24 @@
+package se.hw;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static se.hw.LetterGrade.letterGrade;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Enter the score = ");
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            int score = Integer.parseInt(br.readLine());
+            char grade = letterGrade(score);
+            System.out.println("The grade of " + score + " is " + grade);
+        } catch (NumberFormatException ex) {
+            System.out.println("Not an integer!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
